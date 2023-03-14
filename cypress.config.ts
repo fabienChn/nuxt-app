62dddf3680a9dc3e48533819d0cfc37c3e9e3803
Fabien Cohen <fabiencohen96@gmail.com>
2023-03-14 18:05:30 +0100

    wip fix tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(_on, _config) {
+    baseUrl: "http://localhost:3000",
+    env: {
+      api_url: "http://localhost:4000",
+    },
+    setupNodeEvents(on, _config) {
       // implement node event listeners here
+      // @TODO: to remove if not working
+      let server: any; // static reference to the mock server
+
+      on("task", {
+        mockServer({
+          interceptUrl,
+          fixture,
+        }: {
+          interceptUrl: string;
+          fixture: string;
+        }) {
+          const fs = require("fs");
+          const http = require("http");
+          const { URL } = require("url");
+
+          if (server) server.close(); // close any previous instance
+
+          const url = new URL(interceptUrl);
+          server = http.createServer((req: any, res: any) => {
+            if (req.url === url.pathname) {
+              const data = fs.readFileSync(`./cypress/fixtures/${fixture}`);
+              res.end(data);
+            } else {
+              res.end();
+            }
+          });
+
+          server.listen(url.port);
+
+          return null;
+        },
+      });
     },
   },
 });
--- cypress/e2e/conversations.cy.ts
+++ cypress/e2e/conversations.cy.ts
@@ -3,39 +3,17 @@ import dayjs from "dayjs";
 describe("e2e conversations", () => {
   beforeEach(() => {
     cy.login();
-
-    // @TODO: to remove
-    cy.intercept("GET", "**/*", (req) => {
-      console.log(req.method, req.url);
-      req.continue();
-    }).as("blanket");
   });
 
   describe("When going to the conversations page", () => {
     it.only("renders a list of conversations", () => {
       // @TODO: to fix
-      // cy.intercept(
-      //   {
-      //     method: "GET",
-      //     port: 4000,
-      //     url: "*",
-      //     headers: {
-      //       "Access-Control-Allow-Origin": "*",
-      //     },
-      //   },
-      //   {
-      //     // fixture: "conversations.json",
-      //     body: {
-      //       data: "HELLo",
-      //     },
-      //   }
-      // ).as("convos");
+      cy.task("mockServer", {
+        interceptUrl: "http://localhost:4000/conversations",
+        fixture: "conversations.json",
+      });
 
       cy.visit("http://localhost:3000/conversations");
-
-      // cy.wait("@convos").then((req) => {
-      //   console.log(req);
-      // });
 
       cy.get("div.conversation").should("exist");
     });
@@ -43,6 +21,7 @@ describe("e2e conversations", () => {
     describe("when clicking on a conversation", () => {
       // @TODO: fix
       it.skip("opens the conversation", () => {
+        // TODO: intercept get conversations & get conversations/1 && get messages/1
         cy.visit("http://localhost:3000/conversations");
 
         cy.get("div.conversation").first().click();
@@ -52,9 +31,13 @@ describe("e2e conversations", () => {
 
       describe("when writing text inside the conversation and sending it", () => {
         it("renders in as a new message", () => {
+          // TODO: intercept get conversations/1 & get messages/1
+
           cy.visit("http://localhost:3000/conversations/1");
 
           const text = `message-${dayjs().format("YYYYMMDDHHmmss")}`;
+
+          // TODO: intercept websocket
 
           cy.get("input").wait(50).type(text).type("{enter}");
           cy.get("[data-test=message]").contains(text).should("exist");
